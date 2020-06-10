4964d84667196b602aae221d091578798d94662f
vhp159357 <52731971+vhp159357@users.noreply.github.com>
2020-06-10 08:54:09 +0700

    cap nhat code

--- Lab_WebBanSach/App_Start/BundleConfig.cs
+++ Lab_WebBanSach/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Lab_WebBanSach
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new StyleBundle("~/Content/css/shop-homepage.css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
